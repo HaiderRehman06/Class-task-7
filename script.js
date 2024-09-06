c59d5f9234f4925e6e1579ed53fa348dddc96794
Haider Rehman <haiderrehman500@gmail.com>
2024-09-06 22:39:08 +0500

    updated

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
+var cityName = "Vatican City";
+ function smallestCity() {
+    var selectedButton = event.target.innerText;
+
+if (selectedButton.toLowerCase() === cityName.toLowerCase()) {
+    alert("Correct!");
+        window.location.href = "./second-question.html";
+}else {
+    alert("Incorrect!");
+    window.location.href = "./second-question.html";
+    }
+}
+
+var builingName = "Burj Khalifa";
+function tallestBuiling() {
+    var selectedButton = event.target.innerText;
+
+if (selectedButton.toLowerCase() === builingName.toLowerCase()) {
+    alert("Correct!");
+    window.location.href = "./third-question.html";
+    } else {
+    alert("Incorrect!");
+    window.location.href = "./third-question.html";
+    }
+}
+
+var date = "1995"
+function jsInvented() {
+    var selectedButton = event.target.innerText;
+
+if (selectedButton.toLowerCase() === date.toLowerCase()) {
+    alert("Correct!");
+    } else {
+    alert("Incorrect!");
+    }
+}
+
+
+
+
 
 
